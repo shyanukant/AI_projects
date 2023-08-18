c34ff6ffd7329a341ade3b551c1b21d300d4cf90
Shyanukant Rathi <72182251+shyanukant@users.noreply.github.com>
2023-08-18 06:46:40 +0000

    use meta Llama mdoel

---
+++ Llama_model/fetch_image.py
@@ -0,0 +1,30 @@
+import os
+import requests
+PIXEL_KEY = os.environ.get('PIXEL_KEY')
+def fetch_image(query):
+
+    url = "https://pexelsdimasv1.p.rapidapi.com/v1/search"
+
+    querystring = {"query":query, "per_page":"1"}
+
+    headers = {
+        "Authorization": PIXEL_KEY,
+        "X-RapidAPI-Key": "fa936278admshef9bffe495b2f18p15229bjsnc09f54b19e14",
+        "X-RapidAPI-Host": "PexelsdimasV1.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    if response.status_code  == 200:
+        data = response.json()
+        photos = data.get('photos')
+    
+        if photos :
+            src_original_url = photos[0]['src']['original']
+            return src_original_url
+        else :
+            print('No Image found!!')
+    else:
+        return f"Error : {response.status_code}. {response.text}"
+
+fetch_image("Nature")
